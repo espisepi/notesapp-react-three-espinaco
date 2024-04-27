18e738b75da8a47d1733b03a70d7e5fa4f0901e7
espisepi <sepinaco@gmail.com>
2024-04-27 17:23:22 +0200

    ups

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,14 @@ import Container from "./framework/presentation/components/Container";
 import AddForm from "./framework/presentation/components/AddForm";
 import BookContainer from "./framework/presentation/components/book/BookContainer";
 
-import { Provider } from "react-redux";
-import { store } from "./framework/presentation/store/store";
-
-
 const App = () => {
   return (
     <>
-      <Provider store={store}>
         <Header />
         <Container>
           <AddForm />
           <BookContainer />
         </Container>
-      </Provider>
     </>
   );
 };
