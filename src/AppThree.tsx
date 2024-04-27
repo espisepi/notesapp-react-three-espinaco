aba719d202a204ec39fed1e2d4f239f9fc02d31f
espisepi <sepinaco@gmail.com>
2024-04-27 17:29:26 +0200

    redux using in AppThree

--- src/AppThree.tsx
+++ src/AppThree.tsx
@@ -1,8 +1,12 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import { Model } from './Shoe'
+import { useAppSelector } from './framework/presentation/store/hooks';
 
 export default function AppThree() {
+  // const { isLoading, books } = useAppSelector((state) => state.BookSlice);
+  // console.log("Listado de books de redux: ",{books})
+
   return (
     <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
       <Environment files="./img/forest_slope_1k.hdr" />
